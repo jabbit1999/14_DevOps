bce4aec5c2d0727e19627ac3a4f46b7923e601e6
jabbit1999 <jabbit1999@gmail.com>
2026-02-24 12:38:44 +0900

    2/24 손창우 수업 내용 업로드

---
+++ 07_SonChangWoo/05_message-broker/websocket-stomp-example/src/test/java/com/example/websocketstomp/WebsocketStompExampleApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.websocketstomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketStompExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
